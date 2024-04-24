0cfd91596fdc6b4bf3f0e49d0ba7afdeb5e0fb9e
ofinka08 <bibi.gelingerova@gmail.com>
2024-04-24 09:29:25 +0200

    Fibo hotovo

--- fibo.py
+++ fibo.py
@@ -1,7 +1,20 @@
+def recursive_nth_fibo(n):
+    z = []
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
+
 
 def main():
-    pass
-
+    n = int(input("Zadaj pocet prvkov Fibonnaciho postupnosti: "))
+    # seq = []
+    # for num in range(n):
+    #     seq.append(recursive_nth_fibo(num))
+    seq = [recursive_nth_fibo(num) for num in range(n)]
+    print(seq)
 
 if __name__ == "__main__":
     main()
